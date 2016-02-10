3062ed67c1395b5bf8ba5586e3da0eb08cf7a440
JohnnyMolander <molander.johnny@gmail.com>
2016-02-10 23:33:15 +0100

    initial commit

--- resources/views/emails/order.blade.php
+++ resources/views/emails/order.blade.php
@@ -1,8 +1,29 @@
 <html>
+<head>
+    <style type="text/css">
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        th {
+            height: 50px;
+        }
+
+        th, td {
+            padding: 15px;
+            text-align: left;
+        }
+    </style>
+</head>
 <body>
 <table>
     <thead>
-        <th>Artikel Nummer</th>
+        <th>Artikelnummer</th>
         <th>Antal</th>
         <th>Pris</th>
         <th>Fordon</th>
