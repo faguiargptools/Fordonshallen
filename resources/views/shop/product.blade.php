01c8bdf45482675fd9c99a9fb8875be9b1c38cd6
JohnnyMolander <molander.johnny@gmail.com>
2016-02-11 00:05:23 +0100

    initial commit

--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -59,7 +59,7 @@
                                                         <span class="glyphicon glyphicon-minus"></span>
                                                     </button>
                                                 </span>
-                                                        <input name="quantity" type="text" data-ride="spinner" id="spinner" class="form-control input-number text-center" value="1">
+                                                    <input name="quantity" type="text" data-ride="spinner" id="spinner" class="form-control input-number text-center" value="{{ old('quantity') }}">
                                                 <span class="input-group-btn">
                                                     <button type="button" class="btn btn-default" data-value="increase" data-target="#spinner" data-toggle="spinner">
                                                         <span class="glyphicon glyphicon-plus"></span>
