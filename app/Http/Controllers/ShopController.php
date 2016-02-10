c17703a652e15041d40db76b9a68ad87d0001310
JohnnyMolander <molander.johnny@gmail.com>
2016-02-11 00:26:45 +0100

    initial commit

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -107,7 +107,7 @@ class ShopController extends Controller {
 
 			LaraCart::destroyCart();
 
-			return \Redirect::action('ShopController@orderPlaced', ['placed' => true]);
+			return \Redirect::action('ShopController@orderPlaced')->with('placed', true);
 		}
 	}
 
