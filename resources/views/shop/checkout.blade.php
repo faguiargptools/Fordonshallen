7425c25249d503a170c6227ff0e6b552772c66b7
JohnnyMolander <molander.johnny@gmail.com>
2016-02-10 23:13:24 +0100

    initial commit

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -6,6 +6,20 @@
     active
 @stop
 
+@if (count($errors) > 0)
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+@endif
+
 @section('content')
     <div class="row">
         <div class="col-md-12">
