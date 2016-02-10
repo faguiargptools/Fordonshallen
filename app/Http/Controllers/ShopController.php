5f014e18b3eeca10cc9b54edee200645235d920e
JohnnyMolander <molander.johnny@gmail.com>
2016-02-11 00:14:17 +0100

    initial commit

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -49,7 +49,7 @@ class ShopController extends Controller {
 
 	public function removeItem(){
 		LaraCart::removeItem(Input::get('item'));
-		return Redirect::back()->withInputs();
+		return Redirect::back();
 	}
 
 	public function placeOrder(Request $req){
