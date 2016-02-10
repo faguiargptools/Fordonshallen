0525506c197bc73847f434db3211e56771972041
JohnnyMolander <molander.johnny@gmail.com>
2016-02-10 22:27:36 +0100

    initial commit

--- resources/views/kontaktinformation.blade.php
+++ resources/views/kontaktinformation.blade.php
@@ -7,6 +7,19 @@
 @stop
 
 @section('content')
+@if (count($errors) > 0)
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+@endif
 <div class="row">
     <div class="col-md-12">
         <div id="map" style="width:100%; height:300px;"></div>
